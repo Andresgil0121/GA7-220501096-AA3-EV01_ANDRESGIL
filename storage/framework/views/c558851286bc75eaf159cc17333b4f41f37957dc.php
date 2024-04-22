d8b578feddd250a4c2c3366b02770af05aea716d
AndresGil0121 <105396226+Andresgil0121@users.noreply.github.com>
2024-04-22 11:03:42 -0500

    Add files via upload

---
+++ storage/framework/views/c558851286bc75eaf159cc17333b4f41f37957dc.php
@@ -0,0 +1,12 @@
+<div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="<?php echo e(route('logout')); ?>"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        <?php echo e(__('Logout')); ?>
+
+                                    </a>
+
+                                    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" class="d-none">
+                                        <?php echo csrf_field(); ?>
+                                    </form>
+                                </div><?php /**PATH C:\xampp\htdocs\loginmoderno\resources\views/home.blade.php ENDPATH**/ ?>
